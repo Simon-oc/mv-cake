7dca190d90ae8c5daf2318f3c0eb5415e1ab145f
Simón <simonossacorrea@gmail.com>
2025-03-31 12:50:29 -0500

    Cerrar Sesion Hecho

--- js/cerrarsesion.js
+++ js/cerrarsesion.js
@@ -1,9 +1,10 @@
 const user = JSON.parse(localStorage.getItem("user")) || []
 
 for (let i = 0; i < user.length; i++) {
-    if (user[i].userLogged ) {
-      user[i].userLogged = false
-      localStorage.setItem("user", JSON.stringify(user))
+    if (user[i].logged ) {
+      console.log(user[i].logged)
+      user[i].logged = false
+      localStorage.setItem("user",JSON.stringify(user))
       window.location = "../index.html"
   }
 }
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,8 +1,10 @@
 const user = JSON.parse(localStorage.getItem("user")) || []
-
 const saludoUser = document.querySelector("#saludo-user")
-for (let i = 0; i < array.length; i++) {
+const progreso = document.querySelector("#progreso")
+
+for (let i = 0; i < user.length; i++) {
     if(user[i].logged === true){
-        saludoUser.textContent = `${user[i].user}`
+        saludoUser.textContent = `Hola ${user[i].user}`
+        progreso.textContent = `Progreso: ${user[i].progreso}%`
     }    
 }
