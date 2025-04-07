b297970cee94480168936b675c7e45e3b65cd580
Simón <simonossacorrea@gmail.com>
2025-04-07 13:06:15 -0500

    7/04/2025

--- js/cursos.js
+++ js/cursos.js
@@ -1,16 +1,34 @@
 const modulosVerificacion = document.querySelector("#modulos")
 const users = JSON.parse(localStorage.getItem("user")) || [];
+const linkCerrar = document.querySelector("#cerrar")
+const linkIniciar = document.querySelector("#iniciar")
+const linkCerrar2 = document.querySelector("#cerrar2")
+const linkIniciar2 = document.querySelector("#iniciar2")
+const linkModulo = document.querySelector("#modu-link")
+const linkModulo2 = document.querySelector("#modu-link2")
+
 
 function verificarUsuario(){
-  for (let i = 0; i < users.length; i++) { 
-    if(users[i].logged){ alert("Usuario verificado")
-      modulosVerificacion.style.display = "none"
-    return 
+  for (let i = 0; i < users.length; i++) {
+  if(users[i].logged){
+    console.log("Puedes cerrar sesión")
+    linkCerrar.style.display = "block"
+    linkIniciar.style.display = "none"
+    linkCerrar2.style.display = "block"
+    linkIniciar2.style.display = "none"
+    linkModulo.style.display = "block"
+    linkModulo2.style.display = "block"
+    return
   }
-
-} alert("Sesión no iniciada")
-  modulosVerificacion.style.display = "block"
-  window.location = "../vistas/iniciosesion.html"
+  
+}
+console.log("Debes iniciar sesión")
+linkCerrar.style.display = "none"
+linkIniciar.style.display = "block"
+linkCerrar2.style.display = "none"
+linkIniciar2.style.display = "block"
+linkModulo.style.display = "none"
+linkModulo2.style.display = "none"
 }
 
 function desplegar() {
--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 const users = JSON.parse(localStorage.getItem("user")) || [];
 const linkCerrar = document.querySelector("#cerrar")
 const linkIniciar = document.querySelector("#iniciar")
+const linkCerrar2 = document.querySelector("#cerrar2")
+const linkIniciar2 = document.querySelector("#iniciar2")
 const linkModulo = document.querySelector("#modu-link")
+const linkModulo2 = document.querySelector("#modu-link2")
 
 
 function verificarUsuario(){
@@ -10,7 +13,10 @@ function verificarUsuario(){
     console.log("Puedes cerrar sesión")
     linkCerrar.style.display = "block"
     linkIniciar.style.display = "none"
+    linkCerrar2.style.display = "block"
+    linkIniciar2.style.display = "none"
     linkModulo.style.display = "block"
+    linkModulo2.style.display = "block"
     return
   }
   
@@ -18,7 +24,10 @@ function verificarUsuario(){
 console.log("Debes iniciar sesión")
 linkCerrar.style.display = "none"
 linkIniciar.style.display = "block"
+linkCerrar2.style.display = "none"
+linkIniciar2.style.display = "block"
 linkModulo.style.display = "none"
+linkModulo2.style.display = "none"
 }
 
 function desplegar() {
