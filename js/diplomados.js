e1db5bda64fc2efbe1e35aa80140012e9eaf9007
Simón <simonossacorrea@gmail.com>
2025-04-08 12:49:01 -0500

    Header Justificado

--- js/diplomados.js
+++ js/diplomados.js
@@ -1,3 +1,36 @@
+const modulosVerificacion = document.querySelector("#modulos")
+const users = JSON.parse(localStorage.getItem("user")) || [];
+const linkCerrar = document.querySelector("#cerrar")
+const linkIniciar = document.querySelector("#iniciar")
+const linkCerrar2 = document.querySelector("#cerrar2")
+const linkIniciar2 = document.querySelector("#iniciar2")
+const linkModulo = document.querySelector("#modu-link")
+const linkModulo2 = document.querySelector("#modu-link2")
+
+
+function verificarUsuario(){
+  for (let i = 0; i < users.length; i++) {
+  if(users[i].logged){
+    console.log("Puedes cerrar sesión")
+    linkCerrar.style.display = "block"
+    linkIniciar.style.display = "none"
+    linkCerrar2.style.display = "block"
+    linkIniciar2.style.display = "none"
+    linkModulo.style.display = "block"
+    linkModulo2.style.display = "block"
+    return
+  }
+  
+}
+console.log("Debes iniciar sesión")
+linkCerrar.style.display = "none"
+linkIniciar.style.display = "block"
+linkCerrar2.style.display = "none"
+linkIniciar2.style.display = "block"
+linkModulo.style.display = "none"
+linkModulo2.style.display = "none"
+}
+
 function desplegar() {
     let menu = document.querySelector('.menu')
     menu.classList.toggle('desplegar')
@@ -6,3 +39,5 @@ function desplegar() {
       texto.innerHTML = `<a href="./userpage.html" class="texto">Iniciar Sesión</a>`
     }
   }
+  
+verificarUsuario()
--- js/iniciosesion.js
+++ js/iniciosesion.js
@@ -1,7 +1,48 @@
+const modulosVerificacion = document.querySelector("#modulos")
+const users = JSON.parse(localStorage.getItem("user")) || [];
+const linkCerrar = document.querySelector("#cerrar")
+const linkIniciar = document.querySelector("#iniciar")
+const linkCerrar2 = document.querySelector("#cerrar2")
+const linkIniciar2 = document.querySelector("#iniciar2")
+const linkModulo = document.querySelector("#modu-link")
+const linkModulo2 = document.querySelector("#modu-link2")
 const formRegistro = document.querySelector("#formRegistro")
 const inputUserName = document.querySelector("#inputUserName")
 const inputUserPass = document.querySelector("#inputUserPass")
 
+
+function verificarUsuario(){
+  for (let i = 0; i < users.length; i++) {
+  if(users[i].logged){
+    console.log("Puedes cerrar sesión")
+    linkCerrar.style.display = "block"
+    linkIniciar.style.display = "none"
+    linkCerrar2.style.display = "block"
+    linkIniciar2.style.display = "none"
+    linkModulo.style.display = "block"
+    linkModulo2.style.display = "block"
+    return
+  }
+  
+}
+console.log("Debes iniciar sesión")
+linkCerrar.style.display = "none"
+linkIniciar.style.display = "block"
+linkCerrar2.style.display = "none"
+linkIniciar2.style.display = "block"
+linkModulo.style.display = "none"
+linkModulo2.style.display = "none"
+}
+
+function desplegar() {
+    let menu = document.querySelector('.menu')
+    menu.classList.toggle('desplegar')
+    if(localStorage.getItem('activo') == 'false') {
+      let texto = document.querySelector('.texto')
+      texto.innerHTML = `<a href="./userpage.html" class="texto">Iniciar Sesión</a>`
+    }
+  }
+  
 const user = JSON.parse(localStorage.getItem("user")) || []
 
 function validarUsuario(e) {
@@ -20,3 +61,4 @@ function validarUsuario(e) {
     formRegistro.reset()
 }
 formRegistro.addEventListener("submit", validarUsuario)
+verificarUsuario()
